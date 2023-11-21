1653fad15abaa712b78a47edb6f4ccfaf7477dbf
aqyuki <yksk171118@gmail.com>
2023-11-21 21:05:25 +0900

    feat: add install scripts

---
+++ install.zsh
@@ -0,0 +1,16 @@
+#! /bin/zsh
+
+zsh_script_root="zsh"
+zsh_script_dir=".zsh.d"
+
+# get install script directory
+script_dir="$(cd "$( dirname "${(%):-%x}" )" && pwd)"
+## get file list
+file_list=(find $script_dir/$zsh_script_root/$zsh_script_dir/)
+
+# create .symbolic links
+ln -s $script_dir/$zsh_script_root/.zshrc $HOME/.zshrc
+ln -s $script_dir/$zsh_script_root/$zsh_script_dir $HOME/
+
+# copy config file
+cp -s $script_dir/lazygit/config.yml $HOME/.config/lazygit/config.yml
