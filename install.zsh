ee6781f4b76e21288a774b0e438d45318b46e52c
aqyuki <yksk171118@gmail.com>
2023-11-21 21:42:55 +0900

    feat: update

--- install.zsh
+++ install.zsh
@@ -3,7 +3,8 @@
 zsh_script_root="zsh"
 zsh_script_dir=".zsh.d"
 
-# get install script directory
+# Install zsh config
+## get install script directory
 script_dir="$(cd "$( dirname "${(%):-%x}" )" && pwd)"
 ## get file list
 file_list=(find $script_dir/$zsh_script_root/$zsh_script_dir/)
@@ -12,5 +13,10 @@ file_list=(find $script_dir/$zsh_script_root/$zsh_script_dir/)
 ln -s $script_dir/$zsh_script_root/.zshrc $HOME/.zshrc
 ln -s $script_dir/$zsh_script_root/$zsh_script_dir $HOME/
 
-# copy config file
+# Install git config
+## copy config file
+cp $script_dir/git/common.ini $HOME/.gitconfig
+
+# Install tool config
+## copy config file
 cp -s $script_dir/lazygit/config.yml $HOME/.config/lazygit/config.yml
