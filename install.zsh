305c8870d8c54b2df132f282f464009196f0470b
aqyuki <yksk171118@gmail.com>
2023-11-25 10:10:46 +0900

    feat: update to create symbolic link

--- install.zsh
+++ install.zsh
@@ -21,3 +21,6 @@ touch $HOME/.gitconfig.local
 # Install tool config
 ## copy config file
 cp -s $script_dir/lazygit/config.yml $HOME/.config/lazygit/config.yml
+
+# create .symbolic links
+ln -s $script_dir/nvim $HOME/.config/
