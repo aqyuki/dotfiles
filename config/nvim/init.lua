d1c0da421a62ab8e728bd4f7ac96d369fbebf48d
aqyuki <aqyuki@aqyuki.dev>
2024-06-29 08:54:57 +0900

    feat(nvim): update

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -52,31 +52,33 @@ require('lazy').setup({
                 }
             })
 
-        local on_attach = function(client)   
-          vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
-        end
+            local on_attach = function(client)
+                vim.lsp.inlay_hint.enable(true, {
+                    bufnr = bufnr
+                })
+            end
 
-        lspconfig.rust_analyzer.setup({
-          on_attach = on_attach,
-          settings = {
-            ["rust-analyzer"] = {
-              imports = {
-                granularity = {
-                  group = "module",
-                },
-                prefix = "self",
-              },
-              cargo = {
-                buildScripts = {
-                  enable = true,
-                },
-              },
-              procMacro = {
-                enable = true
-              },
-            }
-          }
-        })
+            lspconfig.rust_analyzer.setup({
+                on_attach = on_attach,
+                settings = {
+                    ["rust-analyzer"] = {
+                        imports = {
+                            granularity = {
+                                group = "module"
+                            },
+                            prefix = "self"
+                        },
+                        cargo = {
+                            buildScripts = {
+                                enable = true
+                            }
+                        },
+                        procMacro = {
+                            enable = true
+                        }
+                    }
+                }
+            })
         end
     }, { -- completion
         "hrsh7th/nvim-cmp",
@@ -227,45 +229,44 @@ require('lazy').setup({
     }
 })
 
-
 vim.api.nvim_create_autocmd("LspAttach", {
-  group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-  callback = function(ev)
-    vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = "*.go",
-    callback = function()
-        local params = vim.lsp.util.make_range_params()
-        params.context = {
-            only = {"source.organizeImports"}
-        }
-        local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
-        for cid, res in pairs(result or {}) do
-            for _, r in pairs(res.result or {}) do
-                if r.edit then
-                    local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
-                    vim.lsp.util.apply_workspace_edit(r.edit, enc)
+    group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+    callback = function(ev)
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            pattern = "*.go",
+            callback = function()
+                local params = vim.lsp.util.make_range_params()
+                params.context = {
+                    only = {"source.organizeImports"}
+                }
+                local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+                for cid, res in pairs(result or {}) do
+                    for _, r in pairs(res.result or {}) do
+                        if r.edit then
+                            local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+                            vim.lsp.util.apply_workspace_edit(r.edit, enc)
+                        end
+                    end
                 end
+                vim.lsp.buf.format({
+                    async = false
+                })
             end
-        end
-        vim.lsp.buf.format({
-            async = false
         })
-      end
-    })
 
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      pattern = { "*.rs", "*.py", "*.ts" },
-      callback = function()
-        vim.lsp.buf.format({
-          buffer = ev.buf,
-          filter = function(f_client)
-            return f_client.name ~= "null-ls"
-          end,
-          async = false,
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            pattern = {"*.rs", "*.py", "*.ts"},
+            callback = function()
+                vim.lsp.buf.format({
+                    buffer = ev.buf,
+                    filter = function(f_client)
+                        return f_client.name ~= "null-ls"
+                    end,
+                    async = false
+                })
+            end
         })
-      end,
-    })
-  end,
+    end
 })
 
 vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>')
