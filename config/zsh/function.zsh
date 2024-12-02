1452de5a089a183440efbcd4ff52da03517993b4
aqyuki <aqyuki@aqyuki.dev>
2024-12-02 20:18:47 +0900

    feat(zsh): 軽めの変更を入れた
    - ショートカットキーをCtrl + eに変更
    - 実行時にディレクトリを切り替えるように変更

--- config/zsh/function.zsh
+++ config/zsh/function.zsh
@@ -13,16 +13,17 @@ zle -N ghq-fzf
 bindkey '^r' ghq-fzf
 
 # code-repoはvscodeで指定したディレクトリを開きます．
-# Ctrl-shift-rで実行できます．
+# Ctrl-eで実行できます．
 # Ref: https://zenn.dev/nowa0402/articles/5eb780280f2523
 function code-open() {
   local repodir=$(ghq list | fzf --preview "git --git-dir $(ghq root)/{}/.git log --date=short --pretty=format:'-%C(yellow)%d%Creset %s %Cgreen(%cd) %C(bold blue)<%an>%Creset' --color")
   if [ -n "$repodir" ]; then
-  echo "Open VSCode Workspace: '$(ghq root)/$repodir'"
-   code $(ghq root)/$repodir
-   zle accept-line
+    echo "Open VSCode Workspace: '$(ghq root)/$repodir'"
+    BUFFER="cd $(ghq root)/$repodir"
+    code $(ghq root)/$repodir
+    zle accept-line
   fi
   zle -R -c
 }
 zle -N code-open
-bindkey '^R' code-open
+bindkey '^e' code-open
