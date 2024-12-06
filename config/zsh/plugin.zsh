a93d917d672f47b01f8679e56be554507237f8c4
aqyuki <aqyuki@aqyuki.dev>
2024-12-06 19:17:09 +0900

    feat(zsh): プラグインを追加

--- config/zsh/plugin.zsh
+++ config/zsh/plugin.zsh
@@ -11,4 +11,5 @@ source "$XDG_DATA_HOME/zinit/zinit.git/zinit.zsh"
 zinit ice depth=1
 zinit light zdharma-continuum/fast-syntax-highlighting
 zinit light olets/zsh-abbr
+zinit light rupa/z
 unalias zi
