e428108e90ebff0cdcfd56b24740671e10ce9587
aqyuki <aqyuki@aqyuki.dev>
2024-12-07 09:37:21 +0900

    feat(zsh): podmanで入力保管が効くようにした

--- config/zsh/tool.zsh
+++ config/zsh/tool.zsh
@@ -33,3 +33,6 @@ eval "$(task --completion zsh)"
 # fzf
 eval "$(fzf --zsh)"
 export FZF_DEFAULT_OPTS='--height 40% --reverse --border --preview "head -100 {}"'
+
+# podman
+eval $(podman completion zsh)
