20c23ecead1342844c2baeeb8f59d95149264c7c
aqyuki <aqyuki@aqyuki.dev>
2024-12-19 21:00:06 +0900

    feat(mise): miseの設定を追加

--- config/fish/conf.d/002-tool.fish
+++ config/fish/conf.d/002-tool.fish
@@ -4,6 +4,9 @@ set -x GHQ_ROOT $HOME/.projects
 # fzf settings
 set -x FZF_DEFAULT_OPTS '--height 40% --reverse --border'
 
+# mise settings
+mise activate fish | source
+
 # load starship prompt
 set -x STARSHIP_CONFIG $HOME/.config/starship/starship.toml
 starship init fish | source
