2d879a4262235be7c1e4f54108e58c5a2f607376
aqyuki <aqyuki@aqyuki.dev>
2024-12-19 21:01:42 +0900

    feat(mise): miseの入力保管を有効化

--- config/fish/conf.d/002-tool.fish
+++ config/fish/conf.d/002-tool.fish
@@ -6,6 +6,7 @@ set -x FZF_DEFAULT_OPTS '--height 40% --reverse --border'
 
 # mise settings
 mise activate fish | source
+mise completion fish | source
 
 # load starship prompt
 set -x STARSHIP_CONFIG $HOME/.config/starship/starship.toml
