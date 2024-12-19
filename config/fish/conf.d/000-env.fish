b9b9c492606447914bba37bc5b9199c5dd2fefa0
aqyuki <aqyuki@aqyuki.dev>
2024-12-19 20:59:59 +0900

    feat(fish): 環境変数を追加

--- config/fish/conf.d/000-env.fish
+++ config/fish/conf.d/000-env.fish
@@ -3,3 +3,7 @@ set -x XDG_CONFIG_HOME $HOME/.config
 set -x XDG_DATA_HOME $HOME/.local/share
 set -x XDG_CACHE_HOME $HOME/.cache
 set -x XDG_STATE_HOME $HOME/.local/state
+set -x XDG_BIN_HOME $HOME/.local/bin
+
+# Path
+fish_add_path $XDG_BIN_HOME
