60b37b7e6dbff345e1a2de4e367fb845a7b54f38
aqyuki <aqyuki@aqyuki.dev>
2025-03-06 13:48:14 +0900

    chore: フォーマットを修正

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -1,6 +1,5 @@
 return {
-  "neovim/nvim-lspconfig",
-  "williamboman/mason.nvim",
-  "williamboman/mason-lspconfig.nvim",
+	"neovim/nvim-lspconfig",
+	"williamboman/mason.nvim",
+	"williamboman/mason-lspconfig.nvim",
 }
-
