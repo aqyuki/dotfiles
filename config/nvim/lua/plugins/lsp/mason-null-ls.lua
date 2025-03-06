acc743f7a74d0de910a02db288b7aa76c889e8ea
aqyuki <aqyuki@aqyuki.dev>
2025-03-06 18:59:09 +0900

    feat(nvim): nvim-lintからnone-lsへの移行

---
+++ config/nvim/lua/plugins/lsp/mason-null-ls.lua
@@ -0,0 +1,14 @@
+return {
+	"jay-babu/mason-null-ls.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = {
+		"williamboman/mason.nvim",
+		"nvimtools/none-ls.nvim",
+	},
+	config = function(mod)
+		mod.setup({
+			automatic_setup = true,
+			handlers = {},
+		})
+	end,
+}
