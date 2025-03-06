c0ddaf131f0999074ff5a1d188455bd5bdc3580d
aqyuki <aqyuki@aqyuki.dev>
2025-03-06 18:40:48 +0900

    config(nvim): LazyGitをNeovim内から起動できるように

---
+++ config/nvim/lua/plugins/util/lazygit.lua
@@ -0,0 +1,11 @@
+return {
+	"kdheepak/lazygit.nvim",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	lazy = true,
+	keys = {
+		{ mode = "n", "lg", "<cmd>LazyGit<CR>" },
+	},
+	config = function()
+		local lazygit = require("lazygit")
+	end,
+}
