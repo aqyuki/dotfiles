999c675d3657939619819bd6acabbbafc67efdae
aqyuki <aqyuki@aqyuki.dev>
2025-03-06 19:07:46 +0900

    config(nvim): formatをmason-null-lsで行うように

--- config/nvim/lua/plugins/lsp/mason-null-ls.lua
+++ config/nvim/lua/plugins/lsp/mason-null-ls.lua
@@ -1,9 +1,16 @@
+local function format()
+	vim.lsp.buf.format({ async = true })
+end
+
 return {
 	"jay-babu/mason-null-ls.nvim",
 	event = { "BufReadPre", "BufNewFile" },
 	dependencies = {
 		"williamboman/mason.nvim",
 		"nvimtools/none-ls.nvim",
+	},
+	keys = {
+		{ "n", "gf", format },
 	},
 	config = function(mod)
 		mod.setup({
