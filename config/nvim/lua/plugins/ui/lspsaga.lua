e4ff1e430b4fa61d38c005070ef864169621a687
aqyuki <aqyuki@aqyuki.dev>
2025-03-06 15:44:10 +0900

    config(nvim): 表示を拡張するプラグインを追加

---
+++ config/nvim/lua/plugins/ui/lspsaga.lua
@@ -0,0 +1,17 @@
+return {
+	"nvimdev/lspsaga.nvim",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"nvim-tree/nvim-web-devicons",
+	},
+	lazy = true,
+	event = { "LspAttach" },
+	config = function()
+		local lspsaga = require("lspsaga")
+		lspsaga.setup({
+			symbol_in_winbar = {
+				enable = false,
+			},
+		})
+	end,
+}
