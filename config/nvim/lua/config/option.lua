58b24c309045388fd8a248ac140c4fc6cbb32823
aqyuki <aqyuki@aqyuki.dev>
2025-03-10 15:12:25 +0900

    config(nvim): Virtual textを有効化

--- config/nvim/lua/config/option.lua
+++ config/nvim/lua/config/option.lua
@@ -20,4 +20,4 @@ opt.expandtab = true
 vim.cmd.colorscheme("onedark")
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] =
-	vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = false })
+	vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = true })
