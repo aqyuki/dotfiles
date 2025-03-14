1576faa6a3a07cbefa825eb84b48c05bfb2571ee
aqyuki <yksk171118@gmail.com>
2025-03-14 14:11:07 +0900

    fix: ユーティリティ関数を修正

--- config/zsh/util.zsh
+++ config/zsh/util.zsh
@@ -1,10 +1,11 @@
 function __fzf_change_working_directory() {
   local select
   select=$(fd --type d -H -E .git -E node_modules | fzf --preview 'lsd --almost-all --color=always --icon=always --literal --tree --depth 2 {}')
-  if [[ -n "$select" ]]; then
-    cd "$select"
+  if [ -n "$select" ]; then
+    BUFFER="cd '$select'"
+    zle accept-line
   fi
-  zle redisplay
+  zle -R -c
 }
 zle -N __fzf_change_working_directory
 bindkey '^f' __fzf_change_working_directory
@@ -12,10 +13,11 @@ bindkey '^f' __fzf_change_working_directory
 function __fzf_repository_search() {
   local src
   src=$(ghq list | fzf --preview "lsd --almost-all --color=always --ignore-glob='.git' --literal --tree --depth 2 $(ghq root)/{}")
-  if [[ -n "$src" ]]; then
-    cd "$(ghq root)/$src"
+  if [ -n "$src" ]; then
+    BUFFER="cd '$(ghq root)/$src'"
+    zle accept-line
   fi
-  zle redisplay
+  zle -R -c
 }
 zle -N __fzf_repository_search
 bindkey '^r' __fzf_repository_search
