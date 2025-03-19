435fb487bf7b8bc935dbb606590d670dfe947eb6
aqyuki <yksk171118@gmail.com>
2025-03-19 21:54:50 +0900

    update: LSPの設定を移動

--- config/nvim/lua/config/option.lua
+++ config/nvim/lua/config/option.lua
@@ -16,11 +16,6 @@ opt.tabstop = 2
 opt.shiftwidth = 2
 opt.expandtab = true
 
--- LSP settings
-vim.lsp.handlers["textDocument/publishDiagnostics"] =
-  vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = true })
-vim.lsp.inlay_hint.enable(true)
-
 -- auto command
 local autocmd = vim.api.nvim_create_autocmd
 autocmd({ "WinEnter", "FocusGained", "BufEnter" }, {
@@ -34,3 +29,50 @@ autocmd({ "BufNewFile", "BufRead" }, {
     vim.bo.filetype = "wgsl"
   end,
 })
+
+-- LSP settings
+vim.lsp.handlers["textDocument/publishDiagnostics"] =
+  vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = true })
+vim.lsp.inlay_hint.enable(true)
+
+local lspconfig = require("lspconfig")
+local server_settings = {
+  rust_analyzer = function(config)
+    config.settings = {
+      ["rust-analyzer"] = {
+        checkOnSave = {
+          command = "clippy",
+        },
+      },
+    }
+    lspconfig.rust_analyzer.setup(config)
+  end,
+  tsserver = function(config)
+    config.filetypes = {
+      "typescript",
+      "typescriptreact",
+      "typescript.tsx",
+    }
+    lspconfig.tsserver.setup(config)
+  end,
+}
+
+-- default server setup
+local function setup_default(server_name, config)
+  require("lspconfig")[server_name].setup(config)
+end
+
+require("mason-lspconfig").setup_handlers({
+  function(server_name)
+    local default_setting = {
+      capabilities = require("cmp_nvim_lsp").default_capabilities(),
+    }
+
+    if server_settings[server_name] then
+      server_settings[server_name](default_setting)
+    else
+      setup_default(server_name, default_setting)
+    end
+  end,
+})
+vim.diagnostic.config()
