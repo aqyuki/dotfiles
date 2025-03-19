9cf612fdef7db225c9d835bee884efc1d29320c2
aqyuki <yksk171118@gmail.com>
2025-03-19 22:33:27 +0900

    update: 設定を整理

--- config/nvim/lua/plugins/fzf.lua
+++ config/nvim/lua/plugins/fzf.lua
@@ -1,7 +1,18 @@
 return {
   "ibhagwan/fzf-lua",
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  lazy = true,
+  keys = {
+    { mode = "n", "<leader>f", "<cmd>lua require('fzf-lua').files()<CR>" },
+    { mode = "n", "<leader>p", "<cmd>lua require('fzf-lua').buffers()<CR>" },
+    { mode = "n", "<leader>b", "<cmd>lua require('fzf-lua').blines()<CR>" },
+    { mode = "n", "<leader>r", "<cmd>lua require('fzf-lua').lsp_references()<CR>" },
+    { mode = "n", "<leader>d", "<cmd>lua require('fzf-lua').lsp_definitions()<CR>" },
+    { mode = "n", "<leader>D", "<cmd>lua require('fzf-lua').lsp_declarations()<CR>" },
+    { mode = "n", "<leader>i", "<cmd>lua require('fzf-lua').lsp_implementations()<CR>" },
+    { mode = "n", "<leader>s", "<cmd>lua require('fzf-lua').lsp_document_symbols()<CR>" },
+    { mode = "n", "<leader>t", "<cmd>lua require('fzf-lua').lsp_typedefs()<CR>" },
+    { mode = "n", "<leader>l", "<cmd>lua require('fzf-lua').diagnostics_document()<CR>" },
   },
   config = function()
     require("fzf-lua").setup({
@@ -17,17 +28,4 @@ return {
     vim.opt.winblend = 5
     vim.opt.termguicolors = true
   end,
-  lazy = true,
-  keys = {
-    { mode = "n", "<leader>f", "<cmd>lua require('fzf-lua').files()<CR>" },
-    { mode = "n", "<leader>p", "<cmd>lua require('fzf-lua').buffers()<CR>" },
-    { mode = "n", "<leader>b", "<cmd>lua require('fzf-lua').blines()<CR>" },
-    { mode = "n", "<leader>r", "<cmd>lua require('fzf-lua').lsp_references()<CR>" },
-    { mode = "n", "<leader>d", "<cmd>lua require('fzf-lua').lsp_definitions()<CR>" },
-    { mode = "n", "<leader>D", "<cmd>lua require('fzf-lua').lsp_declarations()<CR>" },
-    { mode = "n", "<leader>i", "<cmd>lua require('fzf-lua').lsp_implementations()<CR>" },
-    { mode = "n", "<leader>s", "<cmd>lua require('fzf-lua').lsp_document_symbols()<CR>" },
-    { mode = "n", "<leader>t", "<cmd>lua require('fzf-lua').lsp_typedefs()<CR>" },
-    { mode = "n", "<leader>l", "<cmd>lua require('fzf-lua').diagnostics_document()<CR>" },
-  },
 }
