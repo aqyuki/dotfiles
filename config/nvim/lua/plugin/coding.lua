1f4ef88e86b2a52b5399cfbeee770cf3e5f965c9
aqyuki <yksk171118@gmail.com>
2025-04-04 19:51:30 +0900

    ✨ Toggletermを導入 (#37)
    Neovimからターミナルを操作できるようにした

--- config/nvim/lua/plugin/coding.lua
+++ config/nvim/lua/plugin/coding.lua
@@ -87,4 +87,17 @@ return {
 			},
 		},
 	},
+	{
+		"akinsho/toggleterm.nvim",
+		keys = {
+			{ mode = "n", "<leader>tt", "<cmd>ToggleTerm<CR>" },
+			{ mode = "t", "<leader>tt", "<cmd>ToggleTerm<CR>" },
+		},
+		opts = {
+			direction = "float",
+			float_opts = {
+				border = "curved",
+			},
+		},
+	},
 }
