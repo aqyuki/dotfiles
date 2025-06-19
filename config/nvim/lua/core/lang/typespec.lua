e844fcec7ed04abe28d2de3ed14a1f0e5946509b
しろね <yksk171118@gmail.com>
2025-06-19 22:12:44 +0900

    fix(neovim): :bug: Fix treesitter config (#8)

--- config/nvim/lua/core/lang/typespec.lua
+++ config/nvim/lua/core/lang/typespec.lua
@@ -1,10 +1,5 @@
 return {
   {
-    "nvim-treesitter/nvim-treesitter",
-    optional = true,
-    opts = { ensure_installed = "typespec" },
-  },
-  {
     "mason-org/mason.nvim",
     optional = true,
     opts = { ensure_installed = { "tsp-server" } },
