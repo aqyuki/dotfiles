900ba16fb76a87db3714e241bceeb53bcaa756b7
aqyuki <yksk171118@gmail.com>
2026-04-15 08:07:20 +0900

    fix(fish): Previewwを修正

--- config/fish/functions/__fzf_repository_find.fish
+++ config/fish/functions/__fzf_repository_find.fish
@@ -1,5 +1,5 @@
 function __fzf_repository_find -d "fzfを用いてghqで管理されているリポジトリを検索する"
-    set -l src (ghq list | fzf --preview "bat --color=always --style=header,grid --line-range :80 (ghq root)/{}/README.*")
+    set -l src (ghq list | fzf --preview "bat --color=always --style=header,grid --line-range :80 $(ghq root)/{}/README.*")
     if test -n "$src"
         cd (ghq root)/$src
     end
